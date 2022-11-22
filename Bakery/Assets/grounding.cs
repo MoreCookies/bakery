45ac8366b16da6383e838cae58b6f60378ba672a
MoreCookies <derekrs222@gmail.com>
2022-11-22 12:48:46 -0800

    fixed gravity, beginning bakery

--- Bakery/Assets/grounding.cs
+++ Bakery/Assets/grounding.cs
@@ -8,25 +8,60 @@ public class grounding : MonoBehaviour
     public delegate void groundAct();
     public static event groundAct updateGround;
 
+    public delegate void groundAct2();
+    public static event groundAct updateGround2;
+
+    bool isGrounded = false;
+    bool updatedGrounded = false;
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        /*
+        if(!updatedGrounded)
+        {
+            updateGround2();
+        }
+        updatedGrounded = false;
+        */
+        if (Physics.Raycast(transform.position, Vector3.down, 0.5f))
+        {
+            updateGround();
+        } else
+        {
+            updateGround2();
+        }
+    }
+    /*
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.name != "Capsule" && collision.gameObject.name != "Player")
+        {
+            
+            if (updateGround2 != null)
+            {
+                Debug.Log("changing to false");
+                isGrounded = false;
+                //updatedGrounded = false;
+                updateGround2();
+            }
+
+        }
     }
 
-    private void OnCollisionEnter(Collision collision)
+    private void OnCollisionStay(Collision collision)
     {
-        Debug.Log(collision.gameObject.name);
-        if(collision.gameObject.name != "Capsule")
+        Debug.Log(collision.gameObject.name);   
+        if(collision.gameObject.name != "Capsule" && collision.gameObject.name != "Player")
         {
-            Debug.Log("joe's mama");
-            if(updateGround != null)
+            if (updateGround != null)
             {
-                Debug.Log("change");
+                isGrounded = true;
                 updateGround();
+                updatedGrounded = true;
             }
             
         }
         
     }
+    */
 }
--- Bakery/Assets/tutorialmvmnt.cs
+++ Bakery/Assets/tutorialmvmnt.cs
@@ -9,19 +9,34 @@ public class tutorialmvmnt : MonoBehaviour
 
     public float spd = 2f;
     public float sprspd = 4f;
-    public float gravity = 9.81f;
+    public float OrgGravity = -5;
+    
     public bool isGrounded = false;
     public float baseFOV = 60f;
     public float targetFOV = 100f;
     public float accel = 1f;
+    public float jump = 3f;
+    float fov;
+    bool changingFov = false;
+
+    public Camera camera;
+
+    float gravity;
     private void OnEnable()
     {
         grounding.updateGround += updateGrounded;
+        grounding.updateGround2 += updateGrounded2;
     }
 
     private void OnDisable()
     {
         grounding.updateGround -= updateGrounded;
+        grounding.updateGround2 -= updateGrounded2;
+    }
+
+    private void Start()
+    {
+       
     }
 
     // Update is called once per frame
@@ -29,21 +44,35 @@ public class tutorialmvmnt : MonoBehaviour
     {
         float x = Input.GetAxis("Horizontal"); //mapped to wasd or arrow keys i think arrow keys though
         float z = Input.GetAxis("Vertical");
+        camera.fieldOfView = fov;
 
         Vector3 move = transform.right * x + transform.forward * z;
         if(Input.GetKey(KeyCode.LeftShift))
         {
-
+            controller.Move(move * sprspd * Time.deltaTime);
+            fov = targetFOV;
         } else
         {
+            fov = baseFOV;
             controller.Move(move * spd * Time.deltaTime);
-
         }
+        /*
+        if(Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            Debug.Log("jumping");
+            gravity = jump;
+            isGrounded = false;
+        }
+        */
+        //Debug.Log(isGrounded);
         if(!isGrounded)
         {
             //Physics.gravity = new Vector3(0, gravity, 0);
             gravity -= accel;
             controller.Move(new Vector3(0, gravity*Time.deltaTime, 0));
+        } else
+        {
+            gravity = OrgGravity;
         }
         //Debug.Log(isGrounded);
         
@@ -51,7 +80,13 @@ public class tutorialmvmnt : MonoBehaviour
 
     void updateGrounded()
     {
-        Debug.Log("changings");
+        //Debug.Log("is now true");
         isGrounded = true;
+    }
+
+    void updateGrounded2()
+    {
+        //Debug.Log("is now false");
+        isGrounded = false;
     }
 }
